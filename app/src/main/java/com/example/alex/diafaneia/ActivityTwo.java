6d75f6140a3db42ca5ba8974c40e84c4ff761732
AlexAnth <kurtcobainisalivegrunge@gmail.com>
2016-08-23 09:46:27 +0300

    23/8 CardView Success

--- app/src/main/java/com/example/alex/diafaneia/ActivityTwo.java
+++ app/src/main/java/com/example/alex/diafaneia/ActivityTwo.java
@@ -6,10 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
+
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -23,6 +20,7 @@ import com.example.alex.diafaneia.Model.Sector;
 import com.example.alex.diafaneia.Model.Signer;
 import com.example.alex.diafaneia.Model.Type;
 import com.example.alex.diafaneia.Utils.Constants;
+import com.example.alex.diafaneia.Utils.RVAdapter;
 
 
 import org.json.JSONArray;
@@ -30,24 +28,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
-
 
 /**
  * Created by Alex on 3/8/2016.
  */
 public class ActivityTwo extends AppCompatActivity {
 
+    ArrayList <Object> JsonCollection= new ArrayList();
+
     final String SECTORS_BASE_URL = "http://diafaneia.hellenicparliament.gr//api.ashx?q=sectors";
     final String SIGNERS_BASE_URL = "http://diafaneia.hellenicparliament.gr//api.ashx?q=final-signers";
-    final String DOCUMENT_TYPES_BASE_URL ="http://diafaneia.hellenicparliament.gr//api.ashx?q=document-types";
-    private JsonAdapter adapter;
-    private TextView text ;
-    private ArrayList<Object> JsonCollection = new ArrayList<>() ;
+    final String DOCUMENT_TYPES_BASE_URL = "http://diafaneia.hellenicparliament.gr//api.ashx?q=document-types";
+
+    private RecyclerView mRecyclerView;
+    private RVAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,67 +51,33 @@ public class ActivityTwo extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
-        text = (TextView)findViewById(R.id.card_text);
+
+
         // Get the right Label
         String text = getIntent().getStringExtra("B");
-        Log.v("Διαφάνεια","JSON:"+ text);
-        TextView textUi= (TextView)findViewById(R.id.TextInput);
+        Log.v("Διαφάνεια", "JSON:" + text);
+        TextView textUi = (TextView) findViewById(R.id.TextInput);
         textUi.setText(text);
+        mRecyclerView = (RecyclerView) findViewById(R.id.content);
+        mRecyclerView.setNestedScrollingEnabled(false);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new RVAdapter(JsonCollection);
+        mRecyclerView.setAdapter(mAdapter);
+
+        JsonCollection=downloadAPI(text);
 
 
 
-
-        switch (text) {
-            case Constants.SECTOR_TITLE :
-                downloadAPI(Constants.SECTOR_TITLE);
-                break;
-            case Constants.SIGNER_TITLE :
-                downloadAPI(Constants.SIGNER_TITLE);
-
-                break;
-            case Constants.TYPE_TITLE :
-                downloadAPI(Constants.TYPE_TITLE);
-
-                break;
-            case Constants.DOCUMENT_TITLE :
-                downloadAPI(Constants.DOCUMENT_TITLE);
-
-                break;
-        }
-        Log.v("Διαφάνεια","UpdateUI:"+ "Got Here!");
-
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.content);
-        adapter =  new JsonAdapter(JsonCollection);
-        recyclerView.setAdapter(adapter);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
-        recyclerView.setLayoutManager(layoutManager);
-
-        updateUI();
-        adapter.notifyDataSetChanged();
-
-
-
-      }
-    public void updateUI(){
-        if(JsonCollection.size()>0){
-            Object object = JsonCollection.get(0);
-            if (object.getClass().equals(Sector.class)){
-                text.setText(((Sector) object).getSectorTitle());
-            }else if (object.getClass().equals(Signer.class)){
-                 text.setText(((Signer) object).getSignerFullName());
-            }else if (object.getClass().equals(Document.class)){
-                  text.setText(((Document) object).getDocumentTitle());
-            }else if (object.getClass().equals(Type.class)){
-                   text.setText(((Type) object).getTypeTitle());
-            }
-
-        }
-        Log.v("Διαφάνεια","UpdateUI:"+ "Got Here!");
     }
-    public void downloadAPI(String text){
 
 
-        if(text.equalsIgnoreCase(Constants.SECTOR_TITLE)){            // if sectors
+    private ArrayList downloadAPI(String text) {
+
+        if (text.equalsIgnoreCase(Constants.SECTOR_TITLE)) {            // if sectors
 
             final JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET, SECTORS_BASE_URL, null, new Response.Listener<JSONArray>() {
 
@@ -129,26 +91,27 @@ public class ActivityTwo extends AppCompatActivity {
                             String id = jsonobject.getString("ID");
                             String CleanUrl = jsonobject.getString("CleanUrl");
 
-                            Sector sector = new Sector(name,id,CleanUrl);
+                            Sector sector = new Sector(name, id, CleanUrl);
                             JsonCollection.add(sector);
-                            Log.v("Διαφάνεια","SECTORS:"+ sector.getSectorTitle());
+                            Log.v("Διαφάνεια", "SECTORS:" + sector.getSectorTitle());
                         }
-                    }catch (JSONException e){
+                        mAdapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.v("Διαφάνεια","Err:"+ error.getLocalizedMessage());
+                    Log.v("Διαφάνεια", "Err:" + error.getLocalizedMessage());
                 }
             });
 
             Volley.newRequestQueue(this).add(jsonRequest);
 
-        }else if(text.equalsIgnoreCase(Constants.SIGNER_TITLE)){      // if signers
+        } else if (text.equalsIgnoreCase(Constants.SIGNER_TITLE)) {      // if signers
 
-            final JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET,SIGNERS_BASE_URL, null, new Response.Listener<JSONArray>() {
+            final JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET, SIGNERS_BASE_URL, null, new Response.Listener<JSONArray>() {
 
                 public void onResponse(JSONArray response) {
                     try {
@@ -160,26 +123,27 @@ public class ActivityTwo extends AppCompatActivity {
                             String id = jsonobject.getString("ID");
                             String CleanUrl = jsonobject.getString("CleanUrl");
 
-                            Signer signer = new Signer(name,id,CleanUrl);
+                            Signer signer = new Signer(name, id, CleanUrl);
                             JsonCollection.add(signer);
-                            Log.v("Διαφάνεια","SIGNERS:"+ signer.getSignerFullName());
+                            Log.v("Διαφάνεια", "SIGNERS:" + signer.getSignerFullName());
                         }
-                    }catch (JSONException e){
+                        mAdapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.v("Διαφάνεια","Err:"+ error.getLocalizedMessage());
+                    Log.v("Διαφάνεια", "Err:" + error.getLocalizedMessage());
                 }
             });
 
             Volley.newRequestQueue(this).add(jsonRequest);
 
-        }else if(text.equalsIgnoreCase(Constants.DOCUMENT_TITLE)){    // if documents
+        } else if (text.equalsIgnoreCase(Constants.DOCUMENT_TITLE)) {    // if documents
 
-            final JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET,DOCUMENT_TYPES_BASE_URL, null, new Response.Listener<JSONArray>() {
+            final JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET, DOCUMENT_TYPES_BASE_URL, null, new Response.Listener<JSONArray>() {
 
                 public void onResponse(JSONArray response) {
                     try {
@@ -191,30 +155,30 @@ public class ActivityTwo extends AppCompatActivity {
                             String title = jsonArray.getJSONObject(0).getString("Title");
                             String id = jsonArray.getJSONObject(0).getString("ID");
 
-                            Document document = new Document(title,id);
+                            Document document = new Document(title, id);
                             if (jsonArray.length() == 1) {          // Prevents duplicates
                                 JsonCollection.add(document);
-                                Log.v("Διαφάνεια","DOCUMENTS:"+ document.getDocumentTitle());
+                                Log.v("Διαφάνεια", "DOCUMENTS:" + document.getDocumentTitle());
                             }
 
-                        }
+                        } mAdapter.notifyDataSetChanged();
 
-                    }catch (JSONException e){
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.v("Διαφάνεια","Err:"+ error.getLocalizedMessage());
+                    Log.v("Διαφάνεια", "Err:" + error.getLocalizedMessage());
                 }
             });
 
             Volley.newRequestQueue(this).add(jsonRequest);
 
-        }else if(text.equalsIgnoreCase(Constants.TYPE_TITLE)){        // if types
+        } else if (text.equalsIgnoreCase(Constants.TYPE_TITLE)) {        // if types
 
-            final JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET,DOCUMENT_TYPES_BASE_URL, null, new Response.Listener<JSONArray>() {
+            final JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET, DOCUMENT_TYPES_BASE_URL, null, new Response.Listener<JSONArray>() {
 
                 public void onResponse(JSONArray response) {
                     try {
@@ -222,18 +186,18 @@ public class ActivityTwo extends AppCompatActivity {
                         for (int i = 0; i < jsonarray.length(); i++) {
                             JSONObject jsonobject = jsonarray.getJSONObject(i);
                             JSONArray jsonArray = jsonobject.getJSONArray("HierarchyPath");
-                            if (jsonArray.length()==2){
-                            String title = jsonArray.getJSONObject(1).getString("Title");
-                            String id = jsonArray.getJSONObject(1).getString("ID");
+                            if (jsonArray.length() == 2) {
+                                String title = jsonArray.getJSONObject(1).getString("Title");
+                                String id = jsonArray.getJSONObject(1).getString("ID");
 
-                            Type type = new Type(title,id);
-                            JsonCollection.add(type);
-                            Log.v("Διαφάνεια","TYPES:"+ type.getTypeTitle());
+                                Type type = new Type(title, id);
+                                JsonCollection.add(type);
+                                Log.v("Διαφάνεια", "TYPES:" + type.getTypeTitle());
                             }
 
-                        }
+                        } mAdapter.notifyDataSetChanged();
 
-                    }catch (JSONException e){
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
 
@@ -241,7 +205,7 @@ public class ActivityTwo extends AppCompatActivity {
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.v("Διαφάνεια","Err:"+ error.getLocalizedMessage());
+                    Log.v("Διαφάνεια", "Err:" + error.getLocalizedMessage());
                 }
             });
 
@@ -250,59 +214,6 @@ public class ActivityTwo extends AppCompatActivity {
         }
 
 
-    }
+    return JsonCollection;}
 
-    public class JsonAdapter extends RecyclerView.Adapter<ObjectViewHolder>
-    {
-
-        private ArrayList<Object> JsonList;
-
-        public JsonAdapter (ArrayList<Object> JsonList){
-            this.JsonList=JsonList;
-        }
-        @Override
-        public ObjectViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View card = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_json,parent,false);
-            return new ObjectViewHolder(card);
-        }
-
-        @Override
-        public void onBindViewHolder(ObjectViewHolder holder, int position) {
-            Object obj = JsonList.get(position);
-            Log.v("Διαφάνεια","UpdateUI:"+ "Got Here!");
-            holder.updateUI(obj);
-        }
-
-        @Override
-        public int getItemCount() {
-            return JsonList.size();
-        }
-    }
-
-
-    public class ObjectViewHolder extends RecyclerView.ViewHolder{
-
-        private TextView ltext;
-
-        public ObjectViewHolder(View itemView){
-            super(itemView);
-            ltext = (TextView) itemView.findViewById(R.id.card_text);
-        }
-
-        public  void updateUI(Object object){
-            Log.v("Διαφάνεια","UpdateUI:"+ "Got Here!");
-
-            if (object.getClass().equals(Sector.class)){
-                ltext.setText(((Sector) object).getSectorTitle());
-            }else if (object.getClass().equals(Signer.class)){
-                 ltext.setText(((Signer) object).getSignerFullName());
-            }else if (object.getClass().equals(Document.class)){
-                ltext.setText(((Document) object).getDocumentTitle());
-            }else if (object.getClass().equals(Type.class)){
-                ltext.setText(((Type) object).getTypeTitle());
-            }
-
-        }
-
-    }
 }
---
+++ app/src/main/java/com/example/alex/diafaneia/Utils/RVAdapter.java
@@ -0,0 +1,97 @@
+package com.example.alex.diafaneia.Utils;
+
+/**
+ * Created by Alex on 22/8/2016.
+ */
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.alex.diafaneia.Model.Document;
+import com.example.alex.diafaneia.Model.Sector;
+import com.example.alex.diafaneia.Model.Signer;
+import com.example.alex.diafaneia.Model.Type;
+import com.example.alex.diafaneia.R;
+
+import java.util.ArrayList;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.DataObjectHolder> {
+
+        private ArrayList<Object> JsonList = new ArrayList<>();
+        private static MyClickListener myClickListener;
+
+        public static class DataObjectHolder extends RecyclerView.ViewHolder
+                implements View
+                .OnClickListener {
+
+            private TextView ltext;
+            CardView cv;
+
+            public DataObjectHolder(View itemView) {
+                super(itemView);
+                ltext = (TextView) itemView.findViewById(R.id.card_text);
+                cv = (CardView) itemView.findViewById(R.id.cv);
+                Log.v("Diafaneia :", "Adding Listener");
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                myClickListener.onItemClick(getAdapterPosition(), v);
+            }
+        }
+
+        public void setOnItemClickListener(MyClickListener myClickListener) {
+            this.myClickListener = myClickListener;
+        }
+
+        public RVAdapter(ArrayList<Object> myDataset) {
+            JsonList = myDataset;
+        }
+
+        @Override
+        public DataObjectHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.card_json, parent, false);
+
+            DataObjectHolder dataObjectHolder = new DataObjectHolder(view);
+            return dataObjectHolder;
+        }
+
+
+    @Override
+        public void onBindViewHolder(DataObjectHolder holder, int position) {
+
+        Object object = JsonList.get(position);
+
+        if (object.getClass().equals(Sector.class)){
+                holder.ltext.setText(((Sector) object).getSectorTitle());
+            }else if (object.getClass().equals(Signer.class)){
+                holder.ltext.setText(((Signer) object).getSignerFullName());
+            }else if (object.getClass().equals(Document.class)){
+                holder.ltext.setText(((Document) object).getDocumentTitle());
+            }else if (object.getClass().equals(Type.class)){
+                holder.ltext.setText(((Type) object).getTypeTitle());
+        }
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return JsonList.size();
+        }
+
+        public interface MyClickListener {
+            public void onItemClick(int position, View v);
+        }
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
